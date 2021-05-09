0f6ced94a6917dfbf8c33c4242b6781e9f9a99e9
shady osama <shadyosamafawzy@gmail.com>
2021-05-09 14:55:11 +0200

    shady

--- Modules/Listing/Resources/views/listing/share_listing_filter.blade.php
+++ Modules/Listing/Resources/views/listing/share_listing_filter.blade.php
@@ -4,7 +4,9 @@
 
             onclick="advanced_search()">@lang('settings.advanced_search')</button>
 
-    <form method="GET" autocomplete="off"   @if( !request()->has('purpose')) style="display: none;opacity:0;transition:0.7s" @endif id="advanced_filter_form">
+    <form method="GET" autocomplete="off"
+          @if( !request()->has('purpose')) style="display: none;opacity:0;transition:0.7s"
+          @endif id="advanced_filter_form">
 
         <div class="row">
             <div class="col-md-6 col-lg-4">
@@ -14,19 +16,21 @@
                         <label class="font-weight-medium text-muted mr-3">@lang('listing.purpose')</label>
                         <div style="display:flex;">
                             <div class="radio mr-3">
-                                <input type="radio" name="purpose" id="searchAnyRadio1" value="" checked >
+                                <input type="radio" name="purpose" id="searchAnyRadio1" value="" checked>
                                 <label for="searchAnyRadio1">
                                     Any
                                 </label>
                             </div>
                             <div class="radio mr-3">
-                                <input type="radio" name="purpose" id="searchRentRadio1" value="rent" @if(request()->has('purpose') && request('purpose') == 'rent') checked @endif>
+                                <input type="radio" name="purpose" id="searchRentRadio1" value="rent"
+                                       @if(request()->has('purpose') && request('purpose') == 'rent') checked @endif>
                                 <label for="searchRentRadio1">
                                     Rent
                                 </label>
                             </div>
                             <div class="radio">
-                                <input type="radio" name="purpose" id="searchSaleRadio2" value="sale" @if(request()->has('purpose') && request('purpose') == 'sale') checked @endif>
+                                <input type="radio" name="purpose" id="searchSaleRadio2" value="sale"
+                                       @if(request()->has('purpose') && request('purpose') == 'sale') checked @endif>
                                 <label for="searchSaleRadio2">
                                     Sale
                                 </label>
@@ -36,14 +40,14 @@
                 </div>
 
 
-
                 <div class="col-sm-12">
                     <div class="form-group">
                         <label for="" class="font-weight-medium text-muted">@lang('listing.location')</label>
                         <select class="form-control select2" name="location" data-toggle="select2">
                             <option></option>
                             @forelse($locations as $location)
-                                <option value="{{$location}}" @if(request()->has('location') && request('location') == $location) selected  @endif> {{$location}} </option>
+                                <option value="{{$location}}"
+                                        @if(request()->has('location') && request('location') == $location) selected @endif> {{$location}} </option>
                             @empty
                             @endforelse
                         </select>
@@ -51,7 +55,9 @@
                 </div>
 
                 <div class="col-sm-12">
-                    <label class="mb-1 font-weight-medium text-muted">@lang('listing.keywords')  <i title="{{trans('listing.keyword_info')}}" class="fas fa-info-circle" data-toggle="tooltip" data-placement="top" ></i> </label>
+                    <label class="mb-1 font-weight-medium text-muted">@lang('listing.keywords') <i
+                                title="{{trans('listing.keyword_info')}}" class="fas fa-info-circle"
+                                data-toggle="tooltip" data-placement="top"></i> </label>
 
                     <input type="text" name="keywords" id="autocomplete-ajax"
                            placeholder="{{trans('listing.keywords')}}"
@@ -71,7 +77,8 @@
                         <select class="form-control select2" name="type" data-toggle="select2">
                             <option></option>
                             @forelse($types as $type)
-                                <option value="{{$type->id ?? ''}}" @if(request()->has('type') && request('type') == $type->id ) selected  @endif> {{$type->{'name_'.app()->getLocale()} ?? ''}} </option>
+                                <option value="{{$type->id ?? ''}}"
+                                        @if(request()->has('type') && request('type') == $type->id ) selected @endif> {{$type->{'name_'.app()->getLocale()} ?? ''}} </option>
                             @empty
                             @endforelse
                         </select>
@@ -82,17 +89,24 @@
                     <div class="form-group">
                         <label class="mb-1 font-weight-medium text-muted">@lang('listing.price')</label>
                         <div class="d-flex">
-                            <div class="mr-1" style="flex:1">
+                            <div class="input-group mb-2">
+
                                 <input type="text" name="price_min" id="autocomplete-ajax"
                                        placeholder="{{trans('listing.min')}}"
                                        class="form-control"
                                        value="{{ request()->has('price_min') ? request()->get('price_min') : '' }}"/>
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text">AED</div>
+                                </div>
                             </div>
-                            <div class="" style="flex:1">
+                            <div class="input-group mb-2">
                                 <input type="text" name="price_max" id="autocomplete-ajax"
                                        placeholder="{{trans('listing.max')}}"
                                        class="form-control"
                                        value="{{ request()->has('price_max') ? request()->get('price_max') : '' }}"/>
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text">AED</div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -105,11 +119,13 @@
                                 <select class="form-control select2" name="min_bed" data-toggle="select2"
                                         data-placeholder="{{trans('listing.min')}}">
                                     <option value=""></option>
-                                    <option @if(request()->has('min_bed') && request('min_bed') == 'studio') selected @endif value="studio"
+                                    <option @if(request()->has('min_bed') && request('min_bed') == 'studio') selected
+                                            @endif value="studio"
                                     >@lang('listing.studio')</option>
 
                                     @for($i = 1;$i <= 20 ;$i++)
-                                        <option @if(request()->has('min_bed') && request('min_bed') == $i) selected @endif value="{{ $i }}">{{ $i }}</option>
+                                        <option @if(request()->has('min_bed') && request('min_bed') == $i) selected
+                                                @endif value="{{ $i }}">{{ $i }}</option>
                                     @endfor
 
                                 </select>
@@ -118,11 +134,13 @@
                                 <select class="form-control select2" name="max_bed" data-toggle="select2"
                                         data-placeholder="{{trans('listing.max')}}">
                                     <option value=""></option>
-                                    <option @if(request()->has('max_bed') && request('max_bed') == 'studio') selected @endif value="studio"
+                                    <option @if(request()->has('max_bed') && request('max_bed') == 'studio') selected
+                                            @endif value="studio"
                                     >@lang('listing.studio')</option>
 
                                     @for($i = 1;$i <= 20 ;$i++)
-                                        <option @if(request()->has('max_bed') && request('max_bed') == $i) selected @endif value="{{ $i }}">{{ $i }}</option>
+                                        <option @if(request()->has('max_bed') && request('max_bed') == $i) selected
+                                                @endif value="{{ $i }}">{{ $i }}</option>
                                     @endfor
 
                                 </select>
@@ -134,17 +152,27 @@
                     <div class="form-group">
                         <label class="mb-1 font-weight-medium text-muted">@lang('listing.area')</label>
                         <div class="d-flex">
-                            <div class="mr-1" style="flex:1">
+                            <div class="input-group mb-2">
+
                                 <input type="text" name="area_min" id="autocomplete-ajax"
                                        placeholder="{{trans('listing.min')}}"
                                        class="form-control"
                                        value="{{ request()->has('area_min') ? request()->get('area_min') : '' }}"/>
+
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text">sqft</div>
+                                </div>
                             </div>
-                            <div class="" style="flex:1">
+                            <div class="input-group mb-2">
+
                                 <input type="text" name="area_max" id="autocomplete-ajax"
                                        placeholder="{{trans('listing.max')}}"
                                        class="form-control"
                                        value="{{ request()->has('area_max') ? request()->get('area_max') : '' }}"/>
+
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text">sqft</div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -201,7 +229,8 @@
                         <select class="form-control select2" name="filter_agency" data-toggle="select2">
                             <option></option>
                             @forelse($agencies as $agency)
-                                <option value="{{$agency->receiver->id ?? ''}}" @if(request()->has('filter_agency') && request('filter_agency') == $agency->receiver->id ) selected  @endif> {{$agency->receiver->{'company_name_'.app()->getLocale()} ?? ''}} </option>
+                                <option value="{{$agency->receiver->id ?? ''}}"
+                                        @if(request()->has('filter_agency') && request('filter_agency') == $agency->receiver->id ) selected @endif> {{$agency->receiver->{'company_name_'.app()->getLocale()} ?? ''}} </option>
                             @empty
                             @endforelse
                         </select>
